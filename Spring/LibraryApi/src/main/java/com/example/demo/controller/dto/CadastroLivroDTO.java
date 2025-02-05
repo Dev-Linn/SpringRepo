09b285229c7b88a4ccc65841034d35bbe1aacb52
Lincoln <linnsilva3636@gmail.com>
2025-02-05 00:06:51 -0300

    CadastroLivroDTO

--- Spring/LibraryApi/src/main/java/com/example/demo/controller/dto/CadastroLivroDTO.java
+++ Spring/LibraryApi/src/main/java/com/example/demo/controller/dto/CadastroLivroDTO.java
@@ -5,5 +5,9 @@ import com.example.demo.model.GeneroLivro;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
-public record CadastroLivroDTO(String isbn, String titulo, LocalDate dataPublicacao, GeneroLivro genero, BigDecimal preco, AutorDTO autor) {
+public record CadastroLivroDTO(String isbn, String titulo,
+                               LocalDate dataPublicacao,
+                               GeneroLivro genero,
+                               BigDecimal preco,
+                               AutorDTO autor) {
 }
