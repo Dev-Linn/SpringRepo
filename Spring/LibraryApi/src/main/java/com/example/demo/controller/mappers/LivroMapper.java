3f786ff311b4749482e36ca13b942c4e04b9bd00
Lincoln <linnsilva3636@gmail.com>
2025-02-10 22:32:56 -0300

    fixErrors

--- Spring/LibraryApi/src/main/java/com/example/demo/controller/mappers/LivroMapper.java
+++ Spring/LibraryApi/src/main/java/com/example/demo/controller/mappers/LivroMapper.java
@@ -5,13 +5,15 @@ import com.example.demo.model.Livro;
 import com.example.demo.repository.AutorRepository;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.springframework.beans.factory.annotation.Autowired;
 
 
 @Mapper(componentModel = "spring")
 public abstract class LivroMapper {
 
 
-     protected AutorRepository autorRepository;
+    @Autowired
+      AutorRepository autorRepository;
 
     @Mapping(target = "autor", expression = "java( autorRepository.findById(dto.idAutor()).orElse(null))")
     public abstract Livro toEntity(CadastroLivroDTO dto);
--- Spring/LibraryApi/src/main/java/com/example/demo/repository/AutorRepository.java
+++ Spring/LibraryApi/src/main/java/com/example/demo/repository/AutorRepository.java
@@ -4,12 +4,14 @@ import com.example.demo.model.Autor;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+@Repository
 public interface AutorRepository extends JpaRepository<Autor, UUID> {
 
     @Query("SELECT a FROM Autor a")
