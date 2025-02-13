6fa6c94493f7cfb70b87ca564cf514dbe19c5427
Lincoln <linnsilva3636@gmail.com>
2025-02-12 23:56:53 -0300

    melhorando o codigo 1

--- Spring/LibraryApi/src/main/java/com/example/demo/repository/AutorRepository.java
+++ Spring/LibraryApi/src/main/java/com/example/demo/repository/AutorRepository.java
@@ -11,6 +11,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+//autorRepository
 @Repository
 public interface AutorRepository extends JpaRepository<Autor, UUID> {
 
